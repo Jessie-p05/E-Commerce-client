013a920f3283ac9ebdb7242be4c44a0549df41dd
Jessie-p05 <jxpei05@gmail.com>
2022-06-22 00:04:44 -0600

    use router to navigate btw pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,31 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
