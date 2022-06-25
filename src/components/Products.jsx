c94bf4a0b5401915e311ed1ee073cb5330a2de9e
Jessie-p05 <jxpei05@gmail.com>
2022-06-25 00:05:40 -0600

    make filter products function on productList page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,38 +3,51 @@ import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
 import React, { useState } from "react";
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = ({cat, filters, sort}) => {
-  console.log(cat, filters, sort)
+const Products = ({ cat, filters, sort }) => {
+  console.log(cat, filters, sort);
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  useEffect(()=>{
-  const getProducts = async () => {
-    try {
-    const res = await axios.get( cat ? `http://localhost:5000/api/products?category=${cat}` :"http://localhost:5000/api/products");
-    // console.log("hiiiiiii",res)
-    setProducts(res.data)
-    }catch(err){
-    console.log("err from axios",err)
-    }
-  }
-  getProducts()
-  },[cat])
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        // console.log("hiiiiiii",res)
+        setProducts(res.data);
+      } catch (err) {
+        console.log("err from axios", err);
+      }
+    };
+    getProducts();
+  }, [cat]);
 
-  
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [cat, filters, products]);
 
   return (
     <Container>
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
     </Container>
