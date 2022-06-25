d0276e20c7588b9bbaf53fe34a1979c265e6779f
Jessie-p05 <jxpei05@gmail.com>
2022-06-24 23:21:00 -0600

    use axios to fetch data and make cat filter work

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import React, { useState } from "react";
+const axios = require('axios').default;
 
 const Container = styled.div`
     padding: 20px;
@@ -9,7 +12,26 @@ const Container = styled.div`
     justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({cat, filters, sort}) => {
+  console.log(cat, filters, sort)
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(()=>{
+  const getProducts = async () => {
+    try {
+    const res = await axios.get( cat ? `http://localhost:5000/api/products?category=${cat}` :"http://localhost:5000/api/products");
+    // console.log("hiiiiiii",res)
+    setProducts(res.data)
+    }catch(err){
+    console.log("err from axios",err)
+    }
+  }
+  getProducts()
+  },[cat])
+
+  
+
   return (
     <Container>
       {popularProducts.map((item) => (
