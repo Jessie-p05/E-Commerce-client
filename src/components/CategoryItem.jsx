6c7a6c3f7182c915c80e54c40d9e50a9aad26af6
Jessie-p05 <jxpei05@gmail.com>
2022-06-24 23:21:47 -0600

    add link to homepage category camponent

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   flex: 1;
@@ -13,7 +16,6 @@ const Image = styled.img`
   height: 100%;
   object-fit: cover;
   ${mobile({ height: "20vh" })}
-
 `;
 
 const Info = styled.div`
@@ -29,27 +31,29 @@ const Info = styled.div`
 `;
 
 const Title = styled.h1`
-    color:white;
-    margin-bottom: 20px;
+  color: white;
+  margin-bottom: 20px;
 `;
 
 const Button = styled.button`
-    border:none;
-    padding: 10px;
-    background-color: white;
-    color:gray;
-    cursor: pointer;
-    font-weight: 600;
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
 `;
 
 const CategoryItem = ({ item }) => {
   return (
     <Container>
+      <Link to={`/products/${item.cat}`}>
       <Image src={item.img} />
       <Info>
         <Title>{item.title}</Title>
         <Button>SHOP NOW</Button>
       </Info>
+      </Link>
     </Container>
   );
 };
