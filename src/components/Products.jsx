83586c6c10ac9735922d1d1ec97a4592f6dd6217
Jessie-p05 <jxpei05@gmail.com>
2022-06-27 16:06:04 -0600

    sort products on productList page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -45,6 +45,23 @@ const Products = ({ cat, filters, sort }) => {
       );
   }, [cat, filters, products]);
 
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createAt - b.createAt)
+      );
+    }
+    if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
       {filteredProducts.map((item) => (
