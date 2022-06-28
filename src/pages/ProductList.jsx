94dd15299e699bae11f52c852c9ff0420bae2e16
Jessie-p05 <jxpei05@gmail.com>
2022-06-27 22:23:53 -0600

    display the catogory title

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -50,13 +50,13 @@ const ProductList = () => {
       [e.target.name]: value,
     });
   };
-  console.log(filters);
+  // console.log(filters);
 
   return (
     <Container>
       <Navbar />
       <Announcement />
-      <Title>Dresses</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
