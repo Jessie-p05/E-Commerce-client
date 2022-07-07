1fd6bddc14b20c6e33d5f5629100654910c77420
Jessie-p05 <jxpei05@gmail.com>
2022-07-07 15:56:38 -0600

    send login request by axios

--- src/redux/loginCall.js
+++ src/redux/loginCall.js
@@ -1,10 +1,13 @@
+import axios from "axios";
 import { publicRequest } from "../requestMethods";
 import { loginFailure, loginStart, loginSuccess } from "./userSlice";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart(user));
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    // const res = await publicRequest.post("/auth/login", user);
+    const res = await axios.post("http://localhost:5000/api/auth/login", user);
+
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure(user));
