478d7c08411fbc42fed75fa84335b92c8e9ada60
Jessie-p05 <jxpei05@gmail.com>
2022-07-07 14:43:57 -0600

    clean up

--- src/redux/loginCall.js
+++ src/redux/loginCall.js
@@ -1,12 +1,12 @@
 import { publicRequest } from "../requestMethods";
-import { loginFailure, loginStart, loginSuccess } from "./userSlice"
+import { loginFailure, loginStart, loginSuccess } from "./userSlice";
 
-export const login = async(dispatch,user)=> {
-   dispatch(loginStart());
-   try{
-    const res = await publicRequest.post("/auth/login",user);
-    dispatch(loginSuccess(res.data))
-   }catch{
-     dispatch(loginFailure)
-   }
-}
+export const login = async (dispatch, user) => {
+  dispatch(loginStart(user));
+  try {
+    const res = await publicRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure(user));
+  }
+};
