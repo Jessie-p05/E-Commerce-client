e6a13afc7276e95cf968c36ae7a4365bd36279c4
Jessie-p05 <jxpei05@gmail.com>
2022-07-11 17:18:27 -0600

    make login persisted by redux-persist

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,34 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userSlice";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 export default configureStore({
   reducer:{
     cart: cartReducer,
-    user: userReducer
-  }
+    user: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
